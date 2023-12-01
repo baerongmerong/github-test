7cc93f5deac2d4223f41e2a988988212451e3f56
baerongmerong <151704516+baerongmerong@users.noreply.github.com>
2023-12-01 11:51:02 +0900

    Add files via upload

---
+++ Calculator.playground/Contents.swift
@@ -0,0 +1,39 @@
+class Calculator {
+    func calculator(operator: , num1: Int, num2: Int) -> Double {
+        let calculator = Calculator()
+        let addResult = add
+        let subResult = sub
+        let multiplyResult = mul
+        let divideResult = div
+        
+        print("addResult : \(addResult)")
+        print("subtractResult : \(subtractResult)")
+        print("multiplyResult : \(multiplyResult)")
+        print("divideResult : \(divideResult)")
+        
+        return 0
+    }
+}
+import add
+
+func calculator(_ num1:Int, _ num2:Int) -> Int{
+    return (num1 + num2)
+}
+
+import sub
+
+func calculator(_ num1:Int, _ num2:Int) -> Int{
+    return (num1 - num2)
+}
+
+import mul
+
+func calculator(_ num1:Int, _ num2:Int) -> Int{
+    return (num1 * num2)
+}
+
+import div
+
+func calculator(_ num1:Int, _ num2:Int) -> Int{
+    return (num1 / num2)
+}
